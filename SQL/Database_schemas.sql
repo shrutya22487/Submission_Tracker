9c22e48c46201139b345af6013423140858f49c2
shrutya22487 <shrutya22487@iiitd.ac.in>
2024-08-17 20:14:43 +0530

    Did basic setup

--- SQL/Database_schemas.sql
+++ SQL/Database_schemas.sql
@@ -4,7 +4,6 @@ CREATE TABLE IF NOT EXISTS Professor
     Name     VARCHAR(50) NOT NULL,
     email_id VARCHAR(50) NOT NULL
 );
-
 CREATE TABLE IF NOT EXISTS Student
 (
     id       SERIAL PRIMARY KEY,
