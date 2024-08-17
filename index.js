f258feecb7d2a872170f0c8f6cc0462041ef808d
shrutya22487 <shrutya22487@iiitd.ac.in>
2024-08-17 19:39:16 +0530

    Did basic setup

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "database",
+    password: "abcd",
+    port: 5432,
+});
+
+app.use(express.static("public"));
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/",(req, res) => {
+   res.render("home.ejs");
+});
+
